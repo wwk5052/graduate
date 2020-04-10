348245ad1fb2d2713483dd1e618444cf6d845a74
王贰拾 <wangwenkai5052@outlook.com>
2020-04-10 23:18:58 +0800

    config

--- administrator/vue.config.js
+++ administrator/vue.config.js
@@ -1,6 +1,6 @@
 console.log('这是当前环境', process.env.NODE_ENV);
 module.exports = {
-    publicPath: '/graduate/administrator/dist', //根路径
+    publicPath: '/', //根路径
     outputDir: 'dist', //打包的时候生成的一个文件名
     assetsDir: 'assets', //静态资源目录(js,css,img,fonts)这些文件都可以写里面
     lintOnSave: false, //是否开启eslint保存检测 ,它的有效值为 true || false || 'error'
