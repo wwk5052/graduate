8e0ecec6d43527b115aa81cd05269c24b1b355e5
王贰拾 <wangwenkai5052@outlook.com>
2020-04-10 11:59:24 +0800

    vue.config.js打包配置

--- font-end/vue.config.js
+++ font-end/vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    publicPath: '/graduate/administrator', //根路径
+    publicPath: '/graduate/font-end', //根路径
     outputDir: 'dist', //打包的时候生成的一个文件名
     assetsDir: 'assets', //静态资源目录(js,css,img,fonts)这些文件都可以写里面
     lintOnSave: false, //是否开启eslint保存检测 ,它的有效值为 true || false || 'error'
